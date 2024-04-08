37e4dc028e77ea01ad5365175cbf77ea82010cf0
Carnage <akotoxmpimbo@gmail.com>
2024-04-08 16:38:18 +0800

    first commit

---
+++ lib/auth_module.dart
@@ -0,0 +1,12 @@
+library auth_module;
+
+import 'package:flutter/material.dart';
+
+class AuthModula extends StatelessWidget {
+  const AuthModula({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
